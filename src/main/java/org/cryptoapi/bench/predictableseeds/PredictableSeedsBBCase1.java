4dc1d30e196aaba5afdd02fafdba4595aa0e86bd
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2019-09-25 15:26:18 -0400

    Update PredictableSeedsBBCase1.java

--- src/main/java/org/cryptoapi/bench/predictableseeds/PredictableSeedsBBCase1.java
+++ src/main/java/org/cryptoapi/bench/predictableseeds/PredictableSeedsBBCase1.java
@@ -6,8 +6,8 @@ public class PredictableSeedsBBCase1 {
 
     public static void main (String [] args){
         SecureRandom sr = new SecureRandom();
-        byte [] bytes = {(byte) 100, (byte) 200};
-        sr.setSeed(bytes);
+        byte [] keyBytes = {(byte) 100, (byte) 200};
+        sr.setSeed(keyBytes);
         //sr.setSeed(456789L); // Noncompliant
         int v = sr.nextInt();
         System.out.println(v);
