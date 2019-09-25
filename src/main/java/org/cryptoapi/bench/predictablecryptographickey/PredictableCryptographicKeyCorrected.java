769b906c83a6723dec217cba7702870742861f44
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2019-09-25 14:44:29 -0400

    Update PredictableCryptographicKeyCorrected.java

--- src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyCorrected.java
+++ src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyCorrected.java
@@ -13,12 +13,12 @@ public class PredictableCryptographicKeyCorrected {
     public static void main(String [] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException, ClassNotFoundException {
         SecureRandom random = new SecureRandom();
         String defaultKey = String.valueOf(random.ints());
-
-        String originalString = "Testing";
         byte[] keyBytes = defaultKey.getBytes();
         keyBytes = Arrays.copyOf(keyBytes,16);
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
 
+        String originalString = "Testing";
+        
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
         String encrypt = Base64.getEncoder().encodeToString(cipher.doFinal(originalString.getBytes("UTF-8")));
