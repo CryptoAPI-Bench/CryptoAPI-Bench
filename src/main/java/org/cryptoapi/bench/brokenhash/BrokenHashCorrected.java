623310a34997dd18ee51c63bba4562704062f1df
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2020-08-31 10:54:01 -0400

    Update BrokenHashCorrected.java

--- src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashCorrected.java
+++ src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashCorrected.java
@@ -6,7 +6,10 @@ import java.security.NoSuchAlgorithmException;
 public class BrokenHashCorrected {
     public static void main (String [] args) {
         try {
+            String name = "abcdef";
             MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(name.getBytes());
+            System.out.println(md.digest());
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
