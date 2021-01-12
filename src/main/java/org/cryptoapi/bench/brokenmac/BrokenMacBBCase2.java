656b091e43f1e6fdecae34ef9bf4abd430e732fe
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2021-01-12 16:14:28 -0500

    Add files via upload

---
+++ src/main/java/org/cryptoapi/bench/brokenmac/BrokenMacBBCase2.java
@@ -0,0 +1,23 @@
+package org.cryptoapi.bench.brokenmac;
+
+import javax.crypto.Mac;
+import java.security.Key;
+import java.security.SecureRandom;
+import javax.crypto.KeyGenerator;
+
+public class BrokenMacBBCase1 {
+   public static void main(String args[]) throws Exception {
+      KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+      SecureRandom secRandom = new SecureRandom();
+      keyGen.init(secRandom);
+	  Key key = keyGen.generateKey();	 
+      
+
+      Mac mac = Mac.getInstance("HmacSHA1");
+      mac.init(key)
+
+      String msg = new String("TSE2021");
+      byte[] bytes = msg.getBytes();      
+      byte[] macResult = mac.doFinal(bytes);
+  }
+}
