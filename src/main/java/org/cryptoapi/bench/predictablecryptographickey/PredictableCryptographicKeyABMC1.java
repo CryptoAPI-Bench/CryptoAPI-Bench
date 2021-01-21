9073f20200e6b45cc6881646a4ca9706341edaa6
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2021-01-21 15:17:51 -0500

    Update PredictableCryptographicKeyABMC1.java

--- src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyABMC1.java
+++ src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyABMC1.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 public class PredictableCryptographicKeyABMC1 {
     public void go(byte [] key) {
+        byte [] keyBytes;
         keyBytes = Arrays.copyOf(key,16);
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
     }
