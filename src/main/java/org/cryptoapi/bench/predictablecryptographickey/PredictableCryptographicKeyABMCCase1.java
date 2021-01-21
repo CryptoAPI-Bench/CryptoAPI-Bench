34195bfaf7f225087e359c2a4e148ffbab8861b8
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2021-01-21 11:12:41 -0500

    Update PredictableCryptographicKeyABMCCase1.java

--- src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyABMCCase1.java
+++ src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyABMCCase1.java
@@ -3,7 +3,7 @@ package org.cryptoapi.bench.predictablecryptographickey;
 public class PredictableCryptographicKeyABMCCase1 {
     public static void main(String [] args){
         PredictableCryptographicKeyABMC1 pc = new PredictableCryptographicKeyABMC1();
-        String key = "defaultkey";
+        byte key[] = {20,10,30,5,5,6,8,7};
         pc.go(key);
     }
 }
