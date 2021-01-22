6100c2e72fd2bfcc7f774736db572a377f8c2511
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2021-01-22 14:25:38 -0500

    Update UntrustedPRNGCase1.java

--- src/main/java/org/cryptoapi/bench/untrustedprng/UntrustedPRNGCase1.java
+++ src/main/java/org/cryptoapi/bench/untrustedprng/UntrustedPRNGCase1.java
@@ -1,13 +1,17 @@
 package org.cryptoapi.bench.untrustedprng;
 
 import java.util.Random;
+import javax.crypto.spec.PBEParameterSpec;
 
 public class UntrustedPRNGCase1 {
     public static void main(String [] args)
     {
-        Random randomGenerator = new Random();
-        int x = randomGenerator.nextInt();
-        System.out.println(x);
+        Random random = new Random();
+        PBEParameterSpec pbeParamSpec = null;
+        byte[] salt = new byte[32];
+        random.nextBytes(salt);
+        int count = 10020;
+        pbeParamSpec = new PBEParameterSpec(salt, count);
     }
 
 
