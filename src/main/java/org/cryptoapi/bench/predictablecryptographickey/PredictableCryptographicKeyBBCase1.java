e6b6b50fef6970151300c1ac2de62e188ba6de19
CryptoAPI-Bench <49401887+CryptoAPI-Bench@users.noreply.github.com>
2021-08-26 13:50:26 -0400

    Update PredictableCryptographicKeyBBCase1.java

--- src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyBBCase1.java
+++ src/main/java/org/cryptoapi/bench/predictablecryptographickey/PredictableCryptographicKeyBBCase1.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 public class PredictableCryptographicKeyBBCase1 {
     public static void main(String [] args){
         byte keyBytes[] = {20,10,30,5,5,6,8,7};
-        
         keyBytes = Arrays.copyOf(keyBytes,16);
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
     }
